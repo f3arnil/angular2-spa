d4a0493c10390ab1ec3b8f0be6b5a59802f31470
Alex Vasilyeu <f3arnil@gmail.com>
2016-02-08 17:55:46 +0300

    Hello world. In progress

--- angular-spa-ui/js/search-app/search-app.js
+++ angular-spa-ui/js/search-app/search-app.js
@@ -1,3 +1,36 @@
 "use strict";
 
-console.log('Hello world from search simple!')
+console.log('Hello world from search simple!');
+
+var meta = require('reflect-metadata');
+var angular = require('angular2/core');
+var loader = require('angular2/bootstrap')
+
+var Service = function () {};
+Service.prototype.greeting = function () {
+    return 'hello';
+};
+
+
+var search = function () {
+    console.log('!!')
+}
+
+var search = angular.
+Component({
+        selector: 'my-app',
+        appInjector: [Service]
+    })
+    .View({
+        template: '<h1>Hello world</h1>'
+    })
+    .Class({
+        constructor: [Service, function (service) {
+            this.greeting = service.greeting();
+    }]
+    });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    loader.bootstrap(search);
+})
