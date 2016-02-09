70e99faa40c71d8c2345c3116d3056da6fa92f5b
Alex Vasilyeu <f3arnil@gmail.com>
2016-02-09 15:32:53 +0300

    Bad Hello world

---
+++ angular-spa-ui/js/search-app/component.js
@@ -0,0 +1,8 @@
+module.exports = 
+    ng.core.Component({
+        selector: 'my-app',
+        template: '<h1>My First Angular 2 App</h1>'
+    })
+    .Class({
+        constructor: function () {}
+    })
--- angular-spa-ui/js/search-app/search-app.js
+++ angular-spa-ui/js/search-app/search-app.js
@@ -2,35 +2,11 @@
 
 console.log('Hello world from search simple!');
 
-var meta = require('reflect-metadata');
-var angular = require('angular2/core');
-var loader = require('angular2/bootstrap')
+var AppComponent = require('./component');
 
-var Service = function () {};
-Service.prototype.greeting = function () {
-    return 'hello';
-};
+var app = {};
+app.AppComponent = AppComponent;
 
-
-var search = function () {
-    console.log('!!')
-}
-
-var search = angular.
-Component({
-        selector: 'my-app',
-        appInjector: [Service]
-    })
-    .View({
-        template: '<h1>Hello world</h1>'
-    })
-    .Class({
-        constructor: [Service, function (service) {
-            this.greeting = service.greeting();
-    }]
-    });
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    loader.bootstrap(search);
-})
+document.addEventListener('DOMContentLoaded', function () {
+    ng.platform.browser.bootstrap(app.AppComponent);
+});
