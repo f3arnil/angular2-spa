ff836a4ab82b97c62e4682a1459f2004c7486e32
Alex Vasilyeu <f3arnil@gmail.com>
2016-02-09 17:51:21 +0300

    Components.Templates

--- angular-spa-ui/js/search-app/component.js
+++ angular-spa-ui/js/search-app/component.js
@@ -1,8 +1,17 @@
-module.exports = 
+'use strict'
+
+var innDir = require('./innDir');
+
+var component =
     ng.core.Component({
-        selector: 'my-app',
-        template: '<h1>My First Angular 2 App</h1>'
+        selector: '[my-app]',
+        template: '<h1>My First Angular 2 App {{myName}}</h1><indir></indir>',
+        directives:[innDir]
     })
     .Class({
-        constructor: function () {}
+        constructor: function () {
+            this.myName = "Alice";
+        }
     })
+
+module.exports = component;
