62fbfcdec3d3fa96f39da16030019a5d7dd18d42
Alex Vasilyeu <f3arnil@gmail.com>
2016-02-16 16:44:17 +0300

    Component ES6 syntax. Injecting http service

--- angular-spa-ui/js/search-app/test.js
+++ angular-spa-ui/js/search-app/test.js
@@ -1,43 +1,51 @@
 'use strict'
 
-import {Component, Injector, provide} from 'angular2/core';
-import {Injectable} from 'angular2/core'
+import {Component, Injectable, Inject} from 'angular2/core';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
 import {Observable} from 'rxjs/Rx';
 
-@
-Component({
-    selector: '[my-app]',
-    viewProviders: [HTTP_PROVIDERS],
-    template: '<h1>My First Angular 2 App by {{ firstName }}</h1><input [(ngModel)]="firstName"><pre>{{result}}</pre>'
-})
-
 export class App {
-    constructor(http: Http) {
+    static get parameters() {
+        return [[Http]];
+    }
+    
+    constructor(http) {
         this.heroesUrl = '/user/' + GLOBAL_USER_ID;
-        console.log(this.heroesUrl);
         this.firstName = 'Alex';
         http.get(this.heroesUrl)
             .map(res => res.json())
-            .subscribe((res:Response) => this.doSomething(res));
-        console.log('lst');
+            .subscribe((res) => this.doSomething(res));
     }
     
     doSomething (res) {
         this.result = JSON.stringify(res);
         console.log(res);
   }
-}
+};
 
-//Injectable()
-//export class App2 {
-//    constructor(http) {
+App.annotations = [
+    new Component({
+        selector: '[my-app]',
+        providers: [ HTTP_PROVIDERS ],
+        template: '<h1>not My First Angular 2 App by {{ firstName }}</h1><input [(ngModel)]="firstName"><pre>{{result}}</pre>'
+    })
+];
+
+
+//@Component({
+//    selector: '[my-app]',
+//    viewProviders: [HTTP_PROVIDERS],
+//    template: '<h1>My First Angular 2 App by {{ firstName }}</h1><input [(ngModel)]="firstName"><pre>{{result}}</pre>'
+//})
+//
+//export class App {
+//    constructor(http: Http) {
 //        this.heroesUrl = '/user/' + GLOBAL_USER_ID;
 //        console.log(this.heroesUrl);
 //        this.firstName = 'Alex';
 //        http.get(this.heroesUrl)
 //            .map(res => res.json())
-//            .subscribe((res) => this.doSomething(res));
+//            .subscribe((res:Response) => this.doSomething(res));
 //        console.log('lst');
 //    }
 //    
@@ -45,17 +53,4 @@ export class App {
 //        this.result = JSON.stringify(res);
 //        console.log(res);
 //  }
-//};
-//
-//App2.annotations = [
-//    new Component({
-//        selector: '[my-app]',
-//        viewProviders: [HTTP_PROVIDERS],
-//        template: '<h1>not My First Angular 2 App by {{ firstName }}</h1><input [(ngModel)]="firstName"><pre>{{result}}</pre>'
-//    })
-//];
-//
-//var injector = Injector.resolveAndCreate([
-// provide("http", {useClass: Http}),
-// App2
-//]);
+//}
